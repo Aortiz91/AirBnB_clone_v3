64c76860b7f718f5fa5902fc26802b18f9102638
root <root@H62.hbtn.io>
2022-02-06 02:57:06 -0300

    Added route stats

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -4,9 +4,29 @@
 
 from api.v1.views import app_views
 from flask import jsonify
+from models import storage
+from models.amenity import Amenity
+from models.city import City
+from models.place import Place
+from models.review import Review
+from models.state import State
+from models.user import User
 
 
-@app_views.route('/status')
+@app_views.route("/status")
 def status():
     """ Return status OK as JSON """
     return jsonify(status="OK")
+
+
+@app_views.route("/stats")
+def stats():
+    """ Return total of each object"""
+    stats = {}
+    stats["amenities"] = storage.count(Amenity)
+    stats["cities"] = storage.count(City)
+    stats["places"] = storage.count(Place)
+    stats["reviews"] = storage.count(Review)
+    stats["states"] = storage.count(State)
+    stats["users"] = storage.count(User)
+    return jsonify(stats)
